ac6a151ecb8bee334e2c1a19a6884074aaed8ee1
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2019-03-04 17:30:42 +0000

    Use host/port provided from the user

--- app/src/main/java/net/madnation/pulseaudio/MainActivity.java
+++ app/src/main/java/net/madnation/pulseaudio/MainActivity.java
@@ -4,9 +4,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+	int getInt(String int_string)
+	{
+		int default_val = -1;
+
+		try
+		{
+			default_val = Integer.parseInt(int_string);
+		}
+		catch(NumberFormatException nfe)
+		{
+		}
+		return default_val;
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -14,6 +29,8 @@ public class MainActivity extends AppCompatActivity
 		setContentView(R.layout.activity_main);
 
 		final Switch stream_switch = findViewById(R.id.stream_switch);
+		final TextView host_text = findViewById(R.id.host_text);
+		final TextView port_text = findViewById(R.id.port_text);
 		stream_switch.setOnClickListener(new View.OnClickListener()
 		{
 			@Override
@@ -22,7 +39,13 @@ public class MainActivity extends AppCompatActivity
 				boolean startStream = stream_switch.isChecked();
 				if(startStream)
 				{
-					int err = NativeLibs.Start();
+					int port = getInt(port_text.getText().toString());
+					if(port < 0)
+					{
+						//TODO error out
+						return;
+					}
+					int err = NativeLibs.Start(host_text.getText().toString(), port);
 					if(err != 0)
 					{
 						stream_switch.setChecked(false);
